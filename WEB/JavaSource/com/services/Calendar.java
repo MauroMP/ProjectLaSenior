fc18359eadaaeeb99304a39231876828177f06ba
MauroMP <43503442+MauroMP@users.noreply.github.com>
2019-10-12 11:11:15 -0300

    WEb

--- WEB/JavaSource/com/services/Calendar.java
+++ WEB/JavaSource/com/services/Calendar.java
@@ -1,109 +1,34 @@
 package com.services;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 import javax.enterprise.context.SessionScoped;
-
-import javax.faces.event.ValueChangeEvent;
 import javax.inject.Named;
 
 @Named("calendar")
 @SessionScoped
-public class Calendar implements Serializable {
-	
-	
-    /**
+public class Calendar implements Serializable{
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private Locale locale;
-    private boolean popup;
-    private String pattern;
-    private Date selectedDate;
-    private boolean showApply = true;
-    private boolean useCustomDayLabels;
-    private boolean disabled = false;
+	private Date date;
+	
+	public void onClick(Date date) {
+		SimpleDateFormat dateFormat= new SimpleDateFormat("dd/MM/YYYY");
+		System.out.println(dateFormat.format(date));
+	}
 
-    public Calendar() {
+	public Date getDate() {
+		return date;
+	}
 
-        locale = Locale.ENGLISH;
-        popup = true;
-        pattern = "d/M/yy HH:mm";
-    }
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
 
-    public Locale getLocale() {
-        return locale;
-    }
-
-    public void setLocale(Locale locale) {
-        this.locale = locale;
-    }
-
-    public boolean isPopup() {
-        return popup;
-    }
-
-    public void setPopup(boolean popup) {
-        this.popup = popup;
-    }
-
-    public String getPattern() {
-        return pattern;
-    }
-
-    public void setPattern(String pattern) {
-        this.pattern = pattern;
-    }
-
-    public void selectLocale(ValueChangeEvent event) {
-
-        String tLocale = (String) event.getNewValue();
-        if (tLocale != null) {
-            String lang = tLocale.substring(0, 2);
-            String country = tLocale.substring(3);
-            locale = new Locale(lang, country, "");
-        }
-    }
-
-    public boolean isUseCustomDayLabels() {
-        return useCustomDayLabels;
-    }
-
-    public void setUseCustomDayLabels(boolean useCustomDayLabels) {
-        this.useCustomDayLabels = useCustomDayLabels;
-    }
-
-    public Date getSelectedDate() {
-        return selectedDate;
-    }
-
-    public void setSelectedDate(Date selectedDate) {
-        this.selectedDate = selectedDate;
-    }
-
-    public boolean isShowApply() {
-        return showApply;
-    }
-
-    public void setShowApply(boolean showApply) {
-        this.showApply = showApply;
-    }
-
-    public boolean isDisabled() {
-        return disabled;
-    }
-
-    public void setDisabled(boolean disabled) {
-        this.disabled = disabled;
-    }
-
-    public boolean isShowApplyAvailable() {
-        return !disabled && popup;
-    }
-
-    public boolean isPatternAvailable() {
-        return !disabled && popup;
-    }
 }
